250af6207d26b4ad08647e5f332c9d413ce5cb4f
filiztugba1 <fcurukcu@gmail.com>
2022-01-09 22:42:50 +0300

    first commit

---
+++ asenkron_programlama.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+main(List<String> args){
+  print("Anne cocuğu eklemk almaya yollar");
+  uzunSurenIslem();
+  print("Peynir zeytin hazırlanır");
+  printt("kahvaltı hazır");
+}
+
+void uzunSurenIslem(){
+  print("cocuk eklemk almak için evden çıkar");
+  sleep(Duration(seconds:10));
+  print("çocuk eklemekle eve giders");
+}
