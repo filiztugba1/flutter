75e7244e5a8ac7957061aa2690a54a9cc26d3092
Filiz Çürükcü <fcurukcu@gmail.com>
2022-01-20 16:13:25 +0300

    degisiklik

--- asenkron/asenkron_programlama.dart
+++ asenkron/asenkron_programlama.dart
@@ -2,13 +2,20 @@ import 'dart:io';
 
 main(List<String> args) {
   print("Anne cocuğu eklemk almaya yollar");
-  uzunSurenIslem();
+  print("cocuk eklemk almak için evden çıkar");
+  Future<String> sonuc = uzunSurenIslem();
+  sonuc.then((String value) => print(value)).catchError((hata) {
+    print(hata);
+  }).whenComplete(() => print("ekmek alma operasyonu bitti"));
   print("Peynir zeytin hazırlanır");
   print("kahvaltı hazır");
 }
 
-void uzunSurenIslem() {
-  print("cocuk eklemk almak için evden çıkar");
-  sleep(Duration(seconds: 10));
-  print("çocuk eklemekle eve giders");
+Future<String> uzunSurenIslem() {
+  Future<String> sonuc = Future.delayed(Duration(seconds: 2), () {
+    return "çocuk eklemekle eve giders";
+    // throw Exception("Bakkalda ekmek yok");
+  });
+  //sleep(Duration(seconds: 10));
+  return sonuc;
 }
